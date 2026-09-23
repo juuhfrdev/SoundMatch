77231785f16a6b126dbc90e23682bff44d3872af
juuhfrdev <juliasouza200213@gmail.com>
2026-09-23 20:35:26 -0300

    arrumando erro no deezer com NestJS API

--- src/app/services/dmusic.ts
+++ src/app/services/dmusic.ts
@@ -6,37 +6,68 @@ import { timeout } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class MusicService {
+
+  private apiUrl = 'https://soundmatch-api-c0tq.onrender.com';
+
   constructor(private http: HttpClient) {}
 
-  private proxyUrl = 'https://corsproxy.io/?';
-
   searchMusic(query: string) {
-    const url = `${this.proxyUrl}https://api.deezer.com/search?q=${encodeURIComponent(query)}`;
-    return this.http.get<any>(url).pipe(timeout(10000));
+    const url = `${this.apiUrl}/music/search`;
+
+    return this.http.get<any>(url, {
+      params: {
+        q: query
+      }
+    }).pipe(
+      timeout(10000)
+    );
   }
 
   getAlbum(albumId: number) {
-    const url = `${this.proxyUrl}https://api.deezer.com/album/${albumId}`;
-    return this.http.get<any>(url).pipe(timeout(10000));
+    const url = `${this.apiUrl}/music/album/${albumId}`;
+
+    return this.http.get<any>(url).pipe(
+      timeout(10000)
+    );
   }
 
   searchByGenreText(genreName: string) {
-    const url = `${this.proxyUrl}https://api.deezer.com/search?q=${encodeURIComponent(genreName)}`;
-    return this.http.get<any>(url).pipe(timeout(10000));
+    const url = `${this.apiUrl}/music/genre/search`;
+
+    return this.http.get<any>(url, {
+      params: {
+        q: genreName
+      }
+    }).pipe(
+      timeout(10000)
+    );
   }
 
   getGenreArtists(genreId: number) {
-    const url = `${this.proxyUrl}https://api.deezer.com/genre/${genreId}/artists`;
-   return this.http.get<any>(url).pipe(timeout(10000));
-}
+    const url = `${this.apiUrl}/music/genre/${genreId}/artists`;
+
+    return this.http.get<any>(url).pipe(
+      timeout(10000)
+    );
+  }
 
   getGenreTracks(genreId: number) {
-    const url = `${this.proxyUrl}https://api.deezer.com/chart/${genreId}/tracks`;
-    return this.http.get<any>(url).pipe(timeout(10000));
-}
+    const url = `${this.apiUrl}/music/genre/${genreId}/tracks`;
+
+    return this.http.get<any>(url).pipe(
+      timeout(10000)
+    );
+  }
 
   searchArtistTracks(artistName: string) {
-    const url = `${this.proxyUrl}https://api.deezer.com/search?q=${encodeURIComponent(`artist:"${artistName}"`)}`;
-    return this.http.get<any>(url).pipe(timeout(10000));
-}
+    const url = `${this.apiUrl}/music/artist/tracks`;
+
+    return this.http.get<any>(url, {
+      params: {
+        name: artistName
+      }
+    }).pipe(
+      timeout(10000)
+    );
+  }
 }
